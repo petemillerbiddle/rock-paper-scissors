cd6d118856a74f8be1557c7880e34701466e991c
petemillerbiddle <petemillerbiddle@gmail.com>
2022-05-01 09:59:32 -0400

    Add 5 round game() function with scorekeeping

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let moves = ["rock", "paper", "scissors"];
+let playerSelection;
+let computerSelection;
+let playerScore = 0;
+let computerScore = 0;
 
 function computerPlay() {
     let moveIndex = Math.floor(3 * Math.random());
@@ -13,20 +17,39 @@ function playRound(playerSelection, computerSelection) { //params are indices
         return "tie!";
     } else if (playerSelection === 0 && computerSelection === 2) {
         console.log("player beats computer with " + moves[playerSelection] + " over " + moves[computerSelection]);
+        playerScore++;
         return "player wins";
     } else if (playerSelection === 2 && computerSelection === 0) {
         console.log("computer beats player with " + moves[computerSelection] + " over " + moves[playerSelection]);
+        computerScore++;
         return "computer wins";
     } else if (playerSelection > computerSelection) {
         console.log("player beats computer with " + moves[playerSelection] + " over " + moves[computerSelection]);
+        playerScore++;
         return "player wins";
     } else {
         console.log("computer beats player with " + moves[computerSelection] + " over " + moves[playerSelection]);
+        computerScore++;
         return "computer wins";
     }
 }
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playerSelection = "scissors";
+        computerSelection = computerPlay();
+        playRound(moves.indexOf(playerSelection), computerSelection);        
+    }
+    if (playerScore > computerScore) {
+        console.log("player wins!");
+    }
+    else if (computerScore > playerScore) {
+        console.log("computer wins!");
+    }
+    else {
+        console.log("tie game!");
+    }
+}
 
-const playerSelection = "scissors";
-const computerSelection = computerPlay();
-playRound(moves.indexOf(playerSelection), computerSelection);
+
+game();
