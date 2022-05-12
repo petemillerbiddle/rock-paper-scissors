01cb198ba8a1527c76788e92610c0661eb88f9dc
petemillerbiddle <petemillerbiddle@gmail.com>
2022-05-11 21:21:10 -0400

    Play round of first to three points in console

--- script.js
+++ script.js
@@ -14,15 +14,22 @@ function playRound(playerSelection, computerSelection) { //params are indices
     //easy mode where player always wins
     if (playerSelection === computerSelection) {
         console.log("tie! both chose " + moves[playerSelection]);
-        return "tie!";
     } else if (playerSelection ===  (computerSelection + 1) % 3) {
         console.log("X player beats computer with " + moves[playerSelection] + " over " + moves[computerSelection]);
         playerScore++;
-        return "player wins";
     } else {
         console.log("O computer beats player with " + moves[computerSelection] + " over " + moves[playerSelection]);
         computerScore++;
-        return "computer wins";
+    }
+    if (playerScore >= 3) {
+        console.log("player wins round");
+        playerScore = 0;
+        computerScore = 0;
+    }
+    else if (computerScore >= 3) {
+        console.log("computer wins round");
+        playerScore = 0;
+        computerScore = 0;
     }
 }
 
@@ -30,7 +37,6 @@ function playRound(playerSelection, computerSelection) { //params are indices
 const buttons = document.querySelectorAll('button');
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        console.log(button.id + " was clicked");
         playRound(moves.indexOf(button.id), computerPlay());
     });
 });
